636c8d6753747ed57e158b414bfee4eb6cb6197a
JamesYu <jamesyu717@163.com>
2017-10-12 02:52:11 +0800

    normal work

--- 002_ans.py
+++ 002_ans.py
@@ -3,24 +3,27 @@
 牛牛想从这 n 个学生中按照顺序选取 k 名学生，
 要求相邻两个学生的位置编号的差不超过 d，
 使得这 k 个学生的能力值的乘积最大，你能返回最大的乘积吗？
-
->输入描述：
-每个输入包含 1 个测试用例。
-每个测试数据的第一行包含一个整数 n (1 <= n <= 50)，表示学生的个数，
-接下来的一行，包含 n 个整数，按顺序表示每个学生的能力值 ai（-50 <= ai <= 50）。
-接下来的一行包含两个整数，k 和 d (1 <= k <= 10, 1 <= d <= 50)。
-
->输出描述：
-输出一行表示最大的乘积。
-
-示例：
-
-输入：
-3
-7 4 7
-2 50
-
-输出：
-49
 '''
 
+
+while 1:
+    try:
+         n = int(input())
+         A = map(int,input().split())
+         k,d = map(int,input().split())
+    except:
+        break
+    B = [[[0] * 2 for j in range(n)] for i in range(k + 1)]
+    for k in range(1, k + 1):
+        for i in range(n):
+            if k == 1 or i == 0:
+                B[k][i][0],B[k][i][1] = A[i], A[i]
+            else:
+                M = []
+                for t in range(1, d + 1):
+                    if i - t < 0:
+                        break
+                    D = B[k - 1][i - t]
+                    M.extend(D[0] * A[i], D[1] * A[i])
+                B[k][i][0], B[k][i][1] = min(M), max(M)
+    print(max([B[k][i][1] for i in range(n)]))
--- 003_ans.py
+++ 003_ans.py
@@ -1,24 +1,50 @@
 '''题目003：（华为）倒转字符串
 写出一个程序，接受一个字符串，然后输出该字符串反转后的字符串。例如：
-
->输入描述：
-输入N个字符
-
->输出描述：
-输出该字符串反转后的字符串
-
->示例：
-
-输入：
-abcd
-
-输出：
-dcba
-
 '''
-# 第一种方法
-def reverse1(s):
-    return s[::-1]
 
-s = input()
-print(reverse1(s))
+# -*- coding: utf-8 -*-
+#python3.6.2,
+#time: 2017.10.12
+
+#第一种方法：简单的步长-1，即字符串的反转（较常用）
+
+def reverse1(str):
+    return str[::-1]
+
+#第二种方法：交换前后字母的位置。注意：range中是字母L减去1，字母L整除2
+
+def reverse2(str):
+    t = list(str)
+    L = len(t)
+    for i,j in zip(range(L-1,0,-1),range(L//2)):
+        t[i],t[j] = t[j],t[i]
+    return "".join(t)
+
+#第三种方法：递归。每次输出一个字符。
+
+def reverse3(str):
+    if len(str) <= 1:
+        return str
+    return reverse3(str[1:]) + str[0]
+
+#第四种方法：双端队列，用extendleft()函数。
+
+from collections import deque
+def reverse4(str):
+    d = deque()
+    d.extendleft(str)
+    return ''.join(d)
+
+#第五种方法：for循环，从左至右输出。
+
+def reverse5(str):
+    #return ''.join(str[len(str)-i] for i in range(1, len(str)+1))
+    return ''.join(str[i] for i in range(len(str)-1,-1,-1))
+
+
+str = input()
+print(reverse1(str))
+print(reverse2(str))
+print(reverse3(str))
+print(reverse4(str))
+print(reverse5(str))
